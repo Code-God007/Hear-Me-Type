99a2ddefd35e3973bb185d8f9814c57c65fdc9fa
Nitish Dhiman <dhimannitish0828@gmail.com>
2019-08-01 12:35:27 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Hear-Me-Type
+
+**Clone and run for a quick way to see Electron in action.**
+
+This is a minimal Electron application based on the [Quick Start Guide](https://electronjs.org/docs/tutorial/quick-start) within the Electron documentation.
+
+## To Use
+
+To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+git clone https://github.com/electron/electron-quick-start
+# Go into the repository
+cd electron-quick-start
+# Install dependencies
+npm install
+# Run the app
+npm start
+```
---
+++ function.js
@@ -0,0 +1,152 @@
+var res = document.getElementById("result");
+
+document.onkeydown = function(e) {
+  switch (e.keyCode) {
+    case 48:
+      document.getElementById("0").play();
+      res.innerHTML = `You Pressed 0`;
+      break;
+    case 49:
+      document.getElementById("1").play();
+      res.innerHTML = `You Pressed 1`;
+      break;
+    case 50:
+      document.getElementById("2").play();
+      res.innerHTML = `You Pressed 2`;
+      break;
+    case 51:
+      document.getElementById("3").play();
+      res.innerHTML = `You Pressed 3`;
+      break;
+    case 52:
+      document.getElementById("4").play();
+      res.innerHTML = `You Pressed 4`;
+      break;
+    case 53:
+      document.getElementById("5").play();
+      res.innerHTML = `You Pressed 5`;
+      break;
+    case 54:
+      document.getElementById("6").play();
+      res.innerHTML = `You Pressed 6`;
+      break;
+    case 55:
+      document.getElementById("7").play();
+      res.innerHTML = `You Pressed 7`;
+      break;
+    case 56:
+      document.getElementById("8").play();
+      res.innerHTML = `You Pressed 8`;
+      break;
+    case 57:
+      document.getElementById("9").play();
+      res.innerHTML = `You Pressed 9`;
+      break;
+    case 65:
+      document.getElementById("A").play();
+      res.innerHTML = `You Pressed A`;
+      break;
+    case 66:
+      document.getElementById("B").play();
+      res.innerHTML = `You Pressed B`;
+      break;
+    case 67:
+      document.getElementById("C").play();
+      res.innerHTML = `You Pressed C`;
+      break;
+    case 68:
+      document.getElementById("D").play();
+      res.innerHTML = `You Pressed D`;
+      break;
+    case 69:
+      document.getElementById("E").play();
+      res.innerHTML = `You Pressed E`;
+      break;
+    case 70:
+      document.getElementById("F").play();
+      res.innerHTML = `You Pressed F`;
+      break;
+    case 71:
+      document.getElementById("G").play();
+      res.innerHTML = `You Pressed G`;
+      break;
+    case 72:
+      document.getElementById("H").play();
+      res.innerHTML = `You Pressed H`;
+      break;
+    case 73:
+      document.getElementById("I").play();
+      res.innerHTML = `You Pressed I`;
+      break;
+    case 74:
+      document.getElementById("J").play();
+      res.innerHTML = `You Pressed J`;
+      break;
+    case 75:
+      document.getElementById("K").play();
+      res.innerHTML = `You Pressed K`;
+      break;
+    case 76:
+      document.getElementById("L").play();
+      res.innerHTML = `You Pressed L`;
+      break;
+    case 77:
+      document.getElementById("M").play();
+      res.innerHTML = `You Pressed M`;
+      break;
+    case 78:
+      document.getElementById("N").play();
+      res.innerHTML = `You Pressed N`;
+      break;
+    case 79:
+      document.getElementById("O").play();
+      res.innerHTML = `You Pressed O`;
+      break;
+    case 80:
+      document.getElementById("P").play();
+      res.innerHTML = `You Pressed P`;
+      break;
+    case 81:
+      document.getElementById("Q").play();
+      res.innerHTML = `You Pressed Q`;
+      break;
+    case 82:
+      document.getElementById("R").play();
+      res.innerHTML = `You Pressed R`;
+      break;
+    case 83:
+      document.getElementById("S").play();
+      res.innerHTML = `You Pressed S`;
+      break;
+    case 84:
+      document.getElementById("T").play();
+      res.innerHTML = `You Pressed T`;
+      break;
+    case 85:
+      document.getElementById("U").play();
+      res.innerHTML = `You Pressed U`;
+      break;
+    case 86:
+      document.getElementById("V").play();
+      res.innerHTML = `You Pressed V`;
+      break;
+    case 87:
+      document.getElementById("W").play();
+      res.innerHTML = `You Pressed W`;
+      break;
+    case 88:
+      document.getElementById("X").play();
+      res.innerHTML = `You Pressed X`;
+      break;
+    case 89:
+      document.getElementById("Y").play();
+      res.innerHTML = `You Pressed Y`;
+      break;
+    case 90:
+      document.getElementById("Z").play();
+      res.innerHTML = `You Pressed Z`;
+      break;
+    default:
+      res.innerHTML = `Key is not found!`;
+  }
+};
